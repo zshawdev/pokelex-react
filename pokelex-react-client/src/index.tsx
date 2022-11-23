505112bfb48abe8f33a05ec9e56aaf3ee65a24fd
Zane <82951334+zshawdev@users.noreply.github.com>
2022-11-22 19:01:45 -0500

    add default style clearing and base css imports

--- pokelex-react-client/src/index.tsx
+++ pokelex-react-client/src/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "./clear.css";
+import "./index.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById("poke"));
 
