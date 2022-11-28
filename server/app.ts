d855928c7e15f0ba77d5854f483a2924185be24e
Zane <82951334+zshawdev@users.noreply.github.com>
2022-11-28 18:19:13 -0500

    serve static image assets from /images path

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ import path from "path";
 const app = express();
 
 app.use("/cries", express.static(path.join(__dirname, "..", "cries")));
+app.use("/images", express.static(path.join(__dirname, "..", "images")));
 app.use(express.json());
 
 export default app;
