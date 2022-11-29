3045655b1a992adb73f2f2dad3b6e47a32065213
Zane <82951334+zshawdev@users.noreply.github.com>
2022-11-29 17:43:40 -0500

    add pokeball component to layout

--- pokelex-react-client/src/layout/Layout.tsx
+++ pokelex-react-client/src/layout/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Background } from "./background";
+import { Pokeball } from "./pokeball";
 
 const Layout: React.FC = () => {
   return (
-    <>
+    <article>
       <Background />
-    </>
+      <Pokeball />
+    </article>
   );
 };
 
---
+++ pokelex-react-client/src/layout/pokeball/Pokeball.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Pokeball.css";
+
+const Pokeball: React.FC = () => <span className="pokeball">&nbsp;</span>;
+
+export default Pokeball;
