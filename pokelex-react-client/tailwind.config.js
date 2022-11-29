6d69e04be6b28ee241deee24ae576a77e20bf396
Zane <82951334+zshawdev@users.noreply.github.com>
2022-11-28 19:39:48 -0500

    fix tailwind configuration

--- pokelex-react-client/tailwind.config.js
+++ pokelex-react-client/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '814px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
     extend: {},
   },
   plugins: [],
-  darkMode: "class",
-  purge: ["./dist/*.html"]
+  darkMode: "class"
 }
