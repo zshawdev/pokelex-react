a4e5a5eb605cd8dbebd2ee44633cc1946532905d
Zane <82951334+zshawdev@users.noreply.github.com>
2022-11-28 19:39:56 -0500

    fix webpack configuration

--- pokelex-react-client/webpack.config.js
+++ pokelex-react-client/webpack.config.js
@@ -1,22 +1,28 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const IS_PROD = process.env.NODE_ENV === "production";
 
 const plugins = [
   new HtmlWebpackPlugin({
-    template: "./src/index.html"
-  })
+    template: "./src/index.html",
+  }),
 ];
 
 const babelLoaderOptions = { plugins: [] };
 
-if(!IS_PROD) {
+if (!IS_PROD) {
+  plugins.push(
+    new CopyPlugin({
+      patterns: [{ from: "./public/**/*", to: "./" }],
+    })
+  );
   plugins.push(new webpack.HotModuleReplacementPlugin());
   plugins.push(new ReactRefreshWebpackPlugin());
-  babelLoaderOptions.plugins.push(require.resolve("react-refresh/babel"))
+  babelLoaderOptions.plugins.push(require.resolve("react-refresh/babel"));
 }
 
 /** @type {import('webpack').Configuration} */
@@ -24,17 +30,17 @@ module.exports = {
   mode: IS_PROD ? "production" : "development",
   entry: "./src/index.tsx",
   devServer: {
-    hot: true
+    hot: true,
   },
   target: "web",
   output: {
     filename: "bundle.[hash].js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   plugins,
   resolve: {
     modules: [__dirname, "src", "node_modules"],
-    extensions: ["*", ".tsx", ".ts", ".js", ".jsx"]
+    extensions: ["*", ".tsx", ".ts", ".js", ".jsx"],
   },
   module: {
     rules: [
@@ -42,16 +48,39 @@ module.exports = {
         test: /\.ts$|tsx/,
         exclude: /node_modules/,
         loader: require.resolve("babel-loader"),
-        options: babelLoaderOptions
+        options: babelLoaderOptions,
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"]
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+        ],
       },
       {
         test: /\.png|svg|jpg|gif|json$/,
-        use: ["file-loader"]
+        use: [
+          {
+            loader: "file-loader",
+            // options: {
+            //   publicPath: path.resolve(__dirname, "/public/img"),
+            // },
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
-    ]
-  }
+    ],
+  },
 };
