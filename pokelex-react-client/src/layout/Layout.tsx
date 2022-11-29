bfadae6f6df9563fa064a830fc539a259cca9476
Zane <82951334+zshawdev@users.noreply.github.com>
2022-11-29 14:09:06 -0500

    add base layout

---
+++ pokelex-react-client/src/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Background } from "./background";
+
+const Layout: React.FC = () => {
+  return (
+    <>
+      <Background />
+    </>
+  );
+};
+
+export default Layout;
---
+++ pokelex-react-client/src/layout/background/Background.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Background.css";
+
+const Background: React.FC = () => {
+  return (<div className="absolute h-full w-full z-0 poke-background" />);
+};
+
+export default Background;
