c222746916647be1b3113a88e460ea1e9cbd7520
Zane <82951334+zshawdev@users.noreply.github.com>
2022-11-29 20:46:47 -0500

    add pokemon context to body object

--- pokelex-react-client/src/layout/body/Body.tsx
+++ pokelex-react-client/src/layout/body/Body.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import usePokemon from "../../hooks/usePokemon";
 
 import "./Body.css";
 
 const Body: React.FC = () => {
+  const { pokemonList, setPokemonList } = usePokemon();
+  const [listCollapsed, setListCollapsed] = useState<boolean>(true);
+
+  const toggleCollapse = () => setListCollapsed(!listCollapsed);
+
   return (
-    <div className="absolute z-10 top-1/2 left-1/2 flex text-white">
-      <span className="block md:hidden">
+    <div className="body w-[44.825rem] md:w-[81.5rem] absolute z-10 top-1/2 left-1/2 flex text-white">
+      <span onClick={toggleCollapse} className="toggle-button block md:hidden absolute top-[2.8rem] left-8 cursor-pointer">
         <span className="icon">&nbsp;</span>
       </span>
     </div>
