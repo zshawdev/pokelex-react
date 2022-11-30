ec03757fc43c71ebbdf4383a852d0b39958a2f80
Zane <82951334+zshawdev@users.noreply.github.com>
2022-11-29 20:18:03 -0500

    add lang support

---
+++ pokelex-react-client/src/components/LangButton.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface LangButtonProps {
+  text: string;
+  onPress: () => void;
+  active?: boolean;
+}
+const LangButton: React.FC<LangButtonProps> = ({ text, onPress, active }) => {
+  const bgStyle = { backgroundColor: `rgba(43,44,44,${active ? "1" : "0.8"})` };
+  return (
+    <button
+      onClick={onPress}
+      style={bgStyle}
+      className="text-white text-[1.8rem] p-4 cursor-pointer border border-[#878787] hover:border-[#bababa] rounded-[2px]"
+    >
+      {text}
+    </button>
+  );
+};
+
+export default LangButton;
---
+++ pokelex-react-client/src/layout/lang/Lang.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import LangButton from "../../components/LangButton";
+import useLanguage, { LangOption } from "../../hooks/useLanguage";
+
+const langMap: Record<LangOption, Record<LangOption, string>> = {
+  en: { en: "ENGLISH", fr: "FRENCH", de: "GERMAN" },
+  fr: { en: "ANGLAIS", fr: "FRANÇAIS", de: "ALLEMAND" },
+  de: { en: "ENGLISCH", fr: "FRANZÖSISCH", de: "DEUTSCH" },
+};
+
+const Lang: React.FC = () => {
+  const { language, setLanguage } = useLanguage();
+
+  return (
+    <div className="flex justify-center gap-16 bg-[rgba(43,33,33,0.3)] py-4 px-16 z-10">
+      {Object.entries(langMap).map(([lang, text]) => (
+        <LangButton
+          key={lang}
+          text={langMap[language][lang as LangOption]}
+          onPress={() => setLanguage(lang as LangOption)}
+          active={language === lang}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Lang;
