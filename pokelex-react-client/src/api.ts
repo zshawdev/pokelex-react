805fb3a727ef513ba884efdbed117a69f90dc080
Zane <82951334+zshawdev@users.noreply.github.com>
2022-11-29 20:45:56 -0500

    add api interaction on client

---
+++ pokelex-react-client/src/api.ts
@@ -0,0 +1,10 @@
+import fetchJson from "./utils/fetch";
+
+// TODO: have this handled using env secrets and insert in CI/CD where appropriate
+const BASE_URL = process.env.NODE_ENV === "production" ? "https://api.pokelex.com/" : "http://localhost:3000/";
+
+export const getAllPokemon = () => fetchJson(BASE_URL + "pokemon/");
+
+export const getPokemon = (id: number) => fetchJson(BASE_URL + "pokemon/" + id);
+
+export const getPokemonList = (id: number) => fetchJson(BASE_URL + "pokemon-list/");
